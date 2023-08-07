b63dca623ca3058d424853a0ba6d6da1336c6ab3
Antony Njoroge Mutharia <antonynjoro@gmail.com>
2023-08-07 12:32:31 -0600

    removed the provile button from the top nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -111,7 +111,7 @@ function NavBar() {
               open={Boolean(anchorEl)}
               onClose={handleClose}
             >
-              <MenuItem onClick={handleClose}>Profile</MenuItem>
+              {/* <MenuItem onClick={handleClose}>Profile</MenuItem> */}
               <MenuItem onClick={handleLogout}>Log Out</MenuItem>
             </Menu>
           </div>
